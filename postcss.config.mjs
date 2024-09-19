3abbc93d28117c2ae717d71c6482c937913df953
unknown <nnaemekafrank400@gmail.com>
2024-09-19 19:00:36 +0100

    added some packages

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,18 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {},
   },
-};
+}
 
-export default config;
+
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+
+
